e77ef0a0fd782c5484c144d71d54a78258d30591
ewenzel1 <69692254+ewenzel1@users.noreply.github.com>
2020-11-16 13:55:45 -0500

    Update Hooty Bot part 1.py

--- Hooty Bot part 1.py
+++ Hooty Bot part 1.py
@@ -2,7 +2,7 @@ import praw
 import os
 import config
 
-reddit = praw.Reddit(client_id= config.client_id, client_secret= config.client_secret, user_agent= config.user_agent)
+reddit = praw.Reddit(client_id= config.client_id, client_secret= config.client_secret, user_agent= config.user_agent, username= config.username, password= config.password)
 subreddit = reddit.subreddit('KSU')
 
 for submission in subreddit.hot(limit=5):
@@ -11,7 +11,7 @@ for submission in subreddit.hot(limit=5):
     print("Score: ", submission.score)
     print("-------------------------\n")
 
-
+# This file runs in addition to a config.py file that includes all of the login information.
 
 
 
